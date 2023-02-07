e9dec4f24b1e79fb1a619cda00c0b6756da5411c
unknown <st13vv@gmail.com>
2023-02-07 16:42:16 +0200

    added category

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar";
 //import PrivateRoute from "./components/PrivateRoute";
 import Explore from "./pages/Explore";
 //import ForgotPassword from "./pages/ForgotPassword";
-//import Category from "./pages/Category";
+import Category from "./pages/Category";
 import PrivateRoute from "./components/PrivateRoute";
 import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
@@ -20,6 +20,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Explore />} />
             <Route path="/offers" element={<Offers />} />
+            <Route path="/category" element={<Category />} />
             <Route path="/sign-in" element={<SignIn />} />
             <Route
               path="/profile"
