16dcfb71c6f0b3d805633d20c526371416e8098a
unknown <st13vv@gmail.com>
2023-02-13 16:39:25 +0200

    updated category

--- frontend/src/pages/Category.jsx
+++ frontend/src/pages/Category.jsx
@@ -1,20 +1,27 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { getListings } from "../features/listings/listingSlice";
 import { toast } from "react-toastify";
 import Spinner from "../components/Spinner";
-//import ListingItem from "../components/ListingItem";
+import ListingItem from "../components/ListingItem";
 
 function Category() {
-  const [listings, setListings] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const { listings } = useSelector((state) => state.listings);
   const [lastFetchedListing, setLastFetchedListing] = useState(null);
+  const dispatch = useDispatch();
 
   const params = useParams();
 
-  useEffect();
+  useEffect(() => {
+    dispatch(getListings());
+  }, [dispatch]);
 
   // Pagination / Load More
   const onFetchMoreListings = async () => {};
+  if (!listings) {
+    return <Spinner />;
+  }
 
   return (
     <div className="category">
@@ -26,25 +33,27 @@ function Category() {
         </p>
       </header>
 
-      {loading ? (
-        <Spinner />
-      ) : listings && listings.length > 0 ? (
-        <>
-          <main>
-            <ul className="categoryListings"></ul>
-          </main>
-
-          <br />
-          <br />
-          {lastFetchedListing && (
-            <p className="loadMore" onClick={onFetchMoreListings}>
-              Load More
-            </p>
-          )}
-        </>
-      ) : (
-        <p>No listings for {params.categoryName}</p>
-      )}
+      <>
+        <main>
+          <ul className="categoryListings"></ul>
+        </main>
+        <ul className="categoryListings">
+          {listings.map((listing) => (
+            <ListingItem
+              listing={listing.data}
+              id={listing.id}
+              key={listing.id}
+            />
+          ))}
+        </ul>
+        <br />
+        <br />
+        {lastFetchedListing && (
+          <p className="loadMore" onClick={onFetchMoreListings}>
+            Load More
+          </p>
+        )}
+      </>
     </div>
   );
 }
