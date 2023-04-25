eb267ced1eeff648a4256cee428e77b6ee7a5885
unknown <st13vv@gmail.com>
2023-04-25 16:08:25 +0300

    update

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -1,19 +1,23 @@
 const multer = require("multer");
 const asyncHandler = require("express-async-handler");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    console.log(req);
-    console.log(file);
-    cb(null, __dirname, "./uploads");
-  },
-  filename: (req, file, cb) => {
-    console.log(req);
-    console.log(file);
-    cb(null, file.fieldname + "-" + Date.now());
-  },
-});
-
-const upload = multer({ storage: storage });
+const upload = multer({ dest: "uploads/" });
 
 module.exports = upload;
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     console.log(req);
+//     console.log(file);
+//     cb(null, __dirname, "./uploads");
+//   },
+//   filename: (req, file, cb) => {
+//     console.log(req);
+//     console.log(file);
+//     cb(null, file.fieldname + "-" + Date.now());
+//   },
+// });
+
+// const upload = multer({ storage: storage });
+
+// module.exports = upload;
